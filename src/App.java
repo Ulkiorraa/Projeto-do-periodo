df852d05f22c2ae2be01442c4013ca4b10410d74
Fellipe J. P. Oliveira <fellipe_ulkiorra@live.com>
2023-03-02 12:25:13 -0300

    primeiro commit conectando ao DB

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import java.sql.Connection;
+
+import db.DB;
+
+public class App {
+    public static void main(String[] args) {
+        
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
+    }
+}
