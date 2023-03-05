2185f30cb0e130cf73f94278b1e4660bdb9ce660
Fellipe J. P. Oliveira <fellipe_ulkiorra@live.com>
2023-03-04 23:18:15 -0300

    formatação de data e cpf sem função de terceiro

--- src/gui/CadastroPessoaController.java
+++ src/gui/CadastroPessoaController.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-import Formatter.*;
+import javafx.scene.control.TextFormatter;
 
 public class CadastroPessoaController {
 
@@ -36,22 +36,59 @@ public class CadastroPessoaController {
     @FXML
     private RadioButton sexoM;
 
-    @FXML
-    private void cpfKeyReleased() {
-        TextFieldFormatter tff = new TextFieldFormatter();
-        tff.setMask("###.###.###-##");
-        tff.setCaracteresValidos("0123456789");
-        tff.setTf(cpf_txt);
-        tff.formatter();
-    }
+    public void initialize() {
+        // Define a máscara do CPF
+        TextFormatter<String> cpfFormatter = new TextFormatter<>(change -> {
+            if (change.isDeleted()) {
+                return change;
+            }
+            String newText = change.getControlNewText();
+            if (newText.matches("\\d{0,3}\\.?\\d{0,3}\\.?\\d{0,3}-?\\d{0,2}")) {
+                if (newText.length() == 4 || newText.length() == 8){
+                    change.setText(".");
+                }else if (newText.length() == 12) {
+                    change.setText("-");
+                }
+                return change;
+            }
+            return null;
+        });
+        cpf_txt.setTextFormatter(cpfFormatter);
 
-    @FXML
-    private void nascKeyReleased() {
-        TextFieldFormatter tff = new TextFieldFormatter();
-        tff.setMask("##/##/####");
-        tff.setCaracteresValidos("0123456789");
-        tff.setTf(nasc_txt);
-        tff.formatter();
+        // Define a máscara do de nascimento
+        TextFormatter<String> nascFormatter = new TextFormatter<>(change -> {
+            if (change.isDeleted()) {
+                return change;
+            }
+            String newText = change.getControlNewText();
+            if (newText.matches("\\d{0,2}\\/?\\d{0,2}\\/?\\d{0,4}")) {
+                if (newText.length() == 3 || newText.length() == 6){
+                    change.setText("/");
+                }
+                return change;
+            }
+            return null;
+        });
+        nasc_txt.setTextFormatter(nascFormatter);
     }
+    /*
+     * @FXML
+     * private void cpfKeyReleased() {
+     * TextFieldFormatter tff = new TextFieldFormatter();
+     * tff.setMask("###.###.###-##");
+     * tff.setCaracteresValidos("0123456789");
+     * tff.setTf(cpf_txt);
+     * tff.formatter();
+     * }
+     * 
+     * @FXML
+     * private void nascKeyReleased() {
+     * TextFieldFormatter tff = new TextFieldFormatter();
+     * tff.setMask("##/##/####");
+     * tff.setCaracteresValidos("0123456789");
+     * tff.setTf(nasc_txt);
+     * tff.formatter();
+     * }
+     */
 
 }
